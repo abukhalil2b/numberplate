31fabd0771011df21bd153fdb764ef330dc377bc
abukhalil2b <abukhalil2b>
2024-01-26 21:12:54 +0400

    f

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/png" href="/img/favicon.png" />
     <title>برنامج طباعة أرقام المركبات</title>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
